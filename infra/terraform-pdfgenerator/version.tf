63c68ba864c38272f2e5e90d3e3568914e2dbcdd
S <sandesh.suresh18@gmail.com>
2026-05-05 14:44:23 +0530

    meandyou

--- infra/terraform-pdfgenerator/version.tf
+++ infra/terraform-pdfgenerator/version.tf
@@ -1,12 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
   backend "s3" {
     bucket       = "fintech-bucket-all"
     key          = "pdf-generator/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
-
   }
-
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
 }
