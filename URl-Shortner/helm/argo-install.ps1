ab368c137ce74a008f68fead1f0abdbbd0b2ff05
S <sandesh.suresh18@gmail.com>
2026-06-19 15:28:46 +0530

    rules are rules

--- URl-Shortner/helm/argo-install.ps1
+++ URl-Shortner/helm/argo-install.ps1
@@ -6,3 +6,11 @@ helm install argocd argo/argo-cd --namespace argocd --create-namespace
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
 helm upgrade --install kube-state-metrics prometheus-community/kube-state-metrics -n monitoring --create-namespace
+
+
+
+Argocd Installtion
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/port", "value": 8081}]'
