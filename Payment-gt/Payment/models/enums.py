e3486b305652a2dc85df8d244a21bc294af9b3be
S <sandesh.suresh18@gmail.com>
2026-07-13 16:02:17 +0530

    noqu

---
+++ Payment-gt/Payment/models/enums.py
@@ -0,0 +1,30 @@
+# models/enums.py
+from enum import Enum
+
+class PaymentStatus(str, Enum):
+    PENDING = "pending"
+    AUTHORIZED = "authorized"
+    CAPTURED = "captured"
+    PARTIALLY_CAPTURED = "partially_captured"
+    VOIDED = "voided"
+    FAILED = "failed"
+    REFUNDED = "refunded"
+    PARTIALLY_REFUNDED = "partially_refunded"
+    EXPIRED = "expired"
+
+class TransactionType(str, Enum):
+    AUTHORIZATION = "authorization"
+    CAPTURE = "capture"
+    VOID = "void"
+    REFUND = "refund"
+
+class FailureCode(str, Enum):
+    INSUFFICIENT_FUNDS = "insufficient_funds"
+    CARD_DECLINED = "card_declined"
+    EXPIRED_CARD = "expired_card"
+    INCORRECT_CVC = "incorrect_cvc"
+    PROCESSING_ERROR = "processing_error"
+    INVALID_AMOUNT = "invalid_amount"
+    ALREADY_CAPTURED = "already_captured"
+    ALREADY_VOIDED = "already_voided"
+    CANNOT_VOID_CAPTURED = "cannot_void_captured"
