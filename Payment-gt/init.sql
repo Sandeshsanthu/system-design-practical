4c1db40b5ee3af35b4ed87aaa0f3b4cab26c8c5e
S <sandesh.suresh18@gmail.com>
2026-07-15 19:39:08 +0530

    nn

--- Payment-gt/init.sql
+++ Payment-gt/init.sql
@@ -5,9 +5,11 @@ CREATE DATABASE customer_db;
 CREATE DATABASE payment_db;
 CREATE DATABASE fraud_db;
 CREATE DATABASE bank_connector_db;
+CREATE DATABASE ledger_db;
 
 GRANT ALL PRIVILEGES ON DATABASE gateway_db TO payments;
 GRANT ALL PRIVILEGES ON DATABASE customer_db TO payments;
 GRANT ALL PRIVILEGES ON DATABASE payment_db TO payments;
 GRANT ALL PRIVILEGES ON DATABASE fraud_db TO payments;
 GRANT ALL PRIVILEGES ON DATABASE bank_connector_db TO payments;
+GRANT ALL PRIVILEGES ON DATABASE ledger_db TO payments;
